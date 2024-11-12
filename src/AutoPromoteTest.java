ba63209bb5a4c0f58deab7c0b7a25f9d87fff6ce
Andreina <125166401+rei-commits@users.noreply.github.com>
2024-11-12 10:22:11 -0500

    initial commit

---
+++ src/AutoPromoteTest.java
@@ -0,0 +1,9 @@
+
+public class AutoPromoteTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/corejava.java
@@ -0,0 +1,9 @@
+
+public class corejava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
